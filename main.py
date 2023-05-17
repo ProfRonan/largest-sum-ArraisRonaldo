e8dbd0ca40152ea91f1eabfe5ab7922087f10962
ArraisRonaldo <128387746+ArraisRonaldo@users.noreply.github.com>
2023-05-17 10:52:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-"""Module with functions."""
-
-
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
-    """Encontra e retorna os dois números que somados dão o maior valor."""
-    primeiro = 0  # o primeiro número da soma
-    segundo = 0  # o segundo número da soma
-    return primeiro, segundo
+    primeiro = 0 
+    segundo = 0
+    numbers.sort()
+    if len(numbers) < 2:
+        return None
+    else:
+
+     primeiro = numbers[-2]
+     segundo = numbers[-1]
+    
+
+     return primeiro, segundo
